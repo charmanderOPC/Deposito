b3899c3ffcff669bb52b553d69723b44a2b7cb5d
dawmi <dawmi@DESKTOP-1KG68F4>
2024-02-14 08:56:31 +0100

    priemr mensaje

---
+++ src/G2I/ccuenta.java
@@ -0,0 +1,14 @@
+package G2I;
+
+public class ccuenta {
+
+	public ccuenta() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
