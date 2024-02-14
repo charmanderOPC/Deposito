b0695e4fe7e70b0ce5f99c02d7a7199be14081ae
Emerson <emersonvillalvalabrador@gmail.com>
2024-02-14 09:13:40 +0100

    Cambio

--- src/G2I/Main.java
+++ src/G2I/Main.java
@@ -16,7 +16,7 @@ public class Main {
         miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = miCuenta.estado();
         System.out.println("El saldo actual es"+ saldoActual );
-System.out.println("prueba");
+
         try {
             miCuenta.retirar(2300);
         } catch (Exception e) {
